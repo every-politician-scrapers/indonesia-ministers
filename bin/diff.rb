f17289d82884b4b557d1d8935768608fe1be19f3
Tony Bowden <tony@tmtm.com>
2021-08-27 22:05:00 +0300

    Skip anyone with an end date
    The official site lists anyone who has been a member during the current
    term, but Wikidata only lists current members. For now, let's restrict
    to the smaller list.

--- bin/diff.rb
+++ bin/diff.rb
@@ -2,6 +2,16 @@
 # frozen_string_literal: true
 
 require 'every_politician_scraper/comparison'
+require 'pry'
 
-diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+# Restrict to only current members
+# TODO: update the SPARQL to include all in the current term instead
+class Comparison < EveryPoliticianScraper::Comparison
+  def external
+    super.delete_if { |row| row[:end_date] }
+  end
+end
+
+diff = Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
